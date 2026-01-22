88b534359281282df391b272a97ce441521c1d71
Adam Myers <adammyers002@gmail.com>
2026-01-22 10:34:05 -0600

    setup get route for sign up page

---
+++ controllers/auth.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const bcrypt = require("bcrypt");
+const User = require("../models/user");
+
+const router = express.Router();
+
+// GET /auth/sign-up
+router.get("/sign-up", (req, res) => {
+  res.render("auth/sign-up");
+});
