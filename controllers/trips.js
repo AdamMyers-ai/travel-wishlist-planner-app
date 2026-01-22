6ace7f81874eba3b10205eb39a7b4e18c1df1643
Adam Myers <adammyers002@gmail.com>
2026-01-22 15:43:39 -0600

    setup GET /trips in trips.js

--- controllers/trips.js
+++ controllers/trips.js
@@ -1,9 +1,19 @@
-const express = require("express")
-const Trip = require("../models/trip")
-const isSignedIn = require("../middleware/isSignedIn")
+const express = require("express");
+const Trip = require("../models/trip");
+const isSignedIn = require("../middleware/isSignedIn");
 
-const router = express.Router()
+const router = express.Router();
 
+// Index - GET /trips
+router.get("/", isSignedIn, async (req, res) => {
+  try {
+    const trips = await Trip.find({ owner: req.session.user._id });
 
+    res.render("trips/index", { trips });
+  } catch (error) {
+    console.log(error);
+    res.redirect("/");
+  }
+});
 
 module.exports = router;
