d0fa185aceabb2b5de567e28f20159e13d5fd3af
Adam Myers <adammyers002@gmail.com>
2026-01-23 14:16:50 -0600

    setup show.ejs for GET /trips/:tripId route

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ app.use((req, res, next) => {
   next();
 });
 
-// Error message handling
+// message available everywhere
 app.use((req, res, next) => {
   res.locals.message = req.session.message;
   req.session.message = null;
