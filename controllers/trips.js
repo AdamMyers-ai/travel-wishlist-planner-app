8ae558c1a600216af442019c9d8672c76de662c9
Adam Myers <adammyers002@gmail.com>
2026-01-23 10:45:47 -0600

    update GET /trips route for embedded trips schema structure

--- controllers/trips.js
+++ controllers/trips.js
@@ -1,24 +1,28 @@
 const express = require("express");
+const router = express.Router();
+
 const User = require("../models/user");
 const isSignedIn = require("../middleware/isSignedIn");
 
-const router = express.Router();
+// Routes
 
 // // Index - GET /trips
-// router.get("/", isSignedIn, async (req, res) => {
-//   try {
-//     const trips = await Trip.find({ owner: req.session.user._id });
+router.get("/", isSignedIn, async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
 
-//     res.render("trips/index", { trips });
-//   } catch (error) {
-//     console.log(error);
-//     res.redirect("/");
-//   }
-// });
+    res.render("trips/index.ejs", {
+      trips: currentUser.trips,
+    });
+  } catch (error) {
+    console.log(error);
+    res.redirect("/");
+  }
+});
 
-// // New - GET /trips/new
-// router.get("/new", isSignedIn, (req, res) => {
-//   res.render("trips/new");
-// });
+// New - GET /trips/new
+router.get("/new", isSignedIn, (req, res) => {
+  res.render("trips/new");
+});
 
 module.exports = router;
